614141c73d3791deb2ab893813dd60f792b99f80
Álvaro Zamora <alvarozajim@gmail.com>
2021-06-02 21:58:01 +0200

    api routes and book object with rest

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const router = express.Router();
+
+// routes
+const books = require('./routes/api/books');
 
 // Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
